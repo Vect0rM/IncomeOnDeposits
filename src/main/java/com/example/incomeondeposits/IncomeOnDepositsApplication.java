baed000592a528fa8df76bf0c1eed0bdd452a36d
Misha <m.skvortsovvv@gmail.com>
2022-06-10 14:48:57 +0300

    Initial commit

---
+++ src/main/java/com/example/incomeondeposits/IncomeOnDepositsApplication.java
@@ -0,0 +1,13 @@
+package com.example.incomeondeposits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomeOnDepositsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncomeOnDepositsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/incomeondeposits/IncomeOnDepositsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.incomeondeposits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomeOnDepositsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
